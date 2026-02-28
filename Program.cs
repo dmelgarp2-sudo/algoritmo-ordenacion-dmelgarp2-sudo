6eb92bc0ea7e8225932834716825fab5d08b106e
dmelgarp2-sudo <dmelgarp2@miumg.edu.gt>
2026-02-28 09:03:43 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,51 @@
 using System;
 
-const int CANTIDAD_ELEMENTOS = 10000;
-Algoritmo ordenador = new Algoritmo();
-
-// 1. Generar datos (Semilla 42 para que todos tengan el mismo desorden)
-int[] datos = ordenador.GenerarNumeros(CANTIDAD_ELEMENTOS);
-
-Console.WriteLine($"--- Auditoría de Algoritmos .NET 10 ---");
-Console.WriteLine($"Procesando: {CANTIDAD_ELEMENTOS:N0} números.");
-
-// 2. El alumno implementa esto en la clase Algoritmo
-ordenador.BubbleSort(datos);
-
-// 3. Validación de integridad
-if (ordenador.EstaOrdenado(datos))
+public class Algoritmo
 {
-    Console.WriteLine("VALIDATION: SUCCESS");
-}
-else
-{
-    Console.WriteLine("VALIDATION: FAILED");
-    Environment.Exit(1); // Crucial para el Autograding de GitHub
+    // 1. Generar numeros con semilla fija (42)
+    public int[] GenerarNumeros(int cantidad)
+    {
+        int[] arreglo = new int[cantidad];
+        Random random = new Random(42);
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = random.Next(0, 100000);
+        }
+
+        return arreglo;
+    }
+
+    // 2. Implementacion de Bubble Sort
+    public void BubbleSort(int[] arreglo)
+    {
+        int n = arreglo.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arreglo[j] > arreglo[j + 1])
+                {
+                    int temporal = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temporal;
+                }
+            }
+        }
+    }
+
+    // 3. Validacion: verifica si esta ordenado
+    public bool EstaOrdenado(int[] arreglo)
+    {
+        for (int i = 0; i < arreglo.Length - 1; i++)
+        {
+            if (arreglo[i] > arreglo[i + 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }

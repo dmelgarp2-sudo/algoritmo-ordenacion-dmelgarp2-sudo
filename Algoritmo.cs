b05f8d0ca97b858422daf075a0e77ba420cbe78b
dmelgarp2-sudo <dmelgarp2@miumg.edu.gt>
2026-02-28 09:01:41 -0600

    Update Algoritmo.cs

--- Algoritmo.cs
+++ Algoritmo.cs
@@ -1,13 +1,14 @@
 using System;
-using System.Linq; // Necesario para Enumerable y ToArray()
+using System.Linq;
 
 public class Algoritmo
 {
     public int[] GenerarNumeros(int n)
     {
-        // Semilla fija (42) garantiza que todos los alumnos ordenen la misma secuencia
         Random r = new Random(42);
-        return Enumerable.Range(0, n).Select(_ => r.Next(0, 50000)).ToArray();
+        return Enumerable.Range(0, n)
+                         .Select(_ => r.Next(0, 50000))
+                         .ToArray();
     }
 
     public bool EstaOrdenado(int[] arr)
@@ -16,7 +17,6 @@ public class Algoritmo
 
         for (int i = 0; i < arr.Length - 1; i++)
         {
-            // Si el actual es mayor al siguiente, no está ordenado
             if (arr[i] > arr[i + 1]) return false;
         }
         return true;
@@ -24,7 +24,19 @@ public class Algoritmo
 
     public void BubbleSort(int[] arr)
     {
-        // TODO: Implementar el algoritmo de Bubble Sort tradicional
+        int n = arr.Length;
 
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temporal = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temporal;
+                }
+            }
+        }
     }
 }
